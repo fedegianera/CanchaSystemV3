0debc6eecb888a087d99450683db97493a12efc6
Ian <ian_huth_@hotmail.com>
2025-09-29 21:58:01 -0300

    authcontroller v1

--- CanchaSystem/src/main/java/com/example/CanchaSystem/controller/AuthController.java
+++ CanchaSystem/src/main/java/com/example/CanchaSystem/controller/AuthController.java
@@ -1,10 +1,21 @@
 package com.example.CanchaSystem.controller;
 
+import com.example.CanchaSystem.dto.request.AuthRequestDTO;
+import com.example.CanchaSystem.dto.response.AuthResponseDTO;
 import com.example.CanchaSystem.service.JWTService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/auth")
@@ -15,4 +26,22 @@ public class AuthController {
 
     @Autowired
     private JWTService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequestDTO authRequestDTO){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequestDTO.username(),authRequestDTO.password())
+        );
+
+        UserDetails user = (UserDetails) authentication.getPrincipal();
+        String token = jwtService.generateToken(user);
+
+        String role = user.getAuthorities().stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.FORBIDDEN, "User has no roles assigned"));
+
+        return ResponseEntity.ok(new AuthResponseDTO(token, role, user.getUsername()));
+    }
+
 }
