783079fa9d97d5f057d86657cf646053438ce865
6-LL <lorenzanilautaro+github@gmail.com>
2026-05-20 06:45:52 -0300

    UserNotFoundException#userId now String

--- CanchaSystem/src/main/java/com/example/CanchaSystem/exception/user/UserNotFoundException.java
+++ CanchaSystem/src/main/java/com/example/CanchaSystem/exception/user/UserNotFoundException.java
@@ -4,16 +4,25 @@ import com.example.CanchaSystem.exception.ResourceNotFoundException;
 import com.example.CanchaSystem.model.Role;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 public class UserNotFoundException extends ResourceNotFoundException {
-    private final Long userId;
+    private final String userId;
     private final Role role;
 
-    public UserNotFoundException(Long userId, Role role) {
+    public UserNotFoundException(String userId, Role role) {
         super(getStringRole(role) + " no encontrado. ID: " + userId);
 
         this.userId = userId;
         this.role = role;
+    }
+
+    public UserNotFoundException(Long userId, Role role) {
+        this(userId.toString(), role);
+    }
+    public UserNotFoundException(UUID userId, Role role) {
+        this(userId.toString(), role);
     }
 
     private static String getStringRole(Role role) {
