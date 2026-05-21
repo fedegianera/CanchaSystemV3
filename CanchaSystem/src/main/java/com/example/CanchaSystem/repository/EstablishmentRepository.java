17369f8d42168157f6079094688114a76a2257d7
6-LL <lorenzanilautaro+github@gmail.com>
2026-05-21 18:54:27 -0300

    EstablishmentRepository#getAllEstablishmentsNames now returns List<EstablishmentNamesDTO>

--- CanchaSystem/src/main/java/com/example/CanchaSystem/repository/EstablishmentRepository.java
+++ CanchaSystem/src/main/java/com/example/CanchaSystem/repository/EstablishmentRepository.java
@@ -1,13 +1,11 @@
 package com.example.CanchaSystem.repository;
 
-import com.example.CanchaSystem.dto.response.CanchaResponseDTO;
-import com.example.CanchaSystem.dto.response.EstablishmentResponseDTO;
+import com.example.CanchaSystem.dto.EstablishmentNamesDTO;
 import com.example.CanchaSystem.model.Establishment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,5 +24,5 @@ public interface EstablishmentRepository  extends JpaRepository<Establishment, L
             SELECT e.id, e.name
             FROM Establishment e
             """)
-    List<Object[]> getAllEstablishmentsNames();
+    List<EstablishmentNamesDTO> getAllEstablishmentsNames();
 }
