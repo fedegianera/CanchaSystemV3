a4e3fd45743089f6032c514dc8f46345bc3be7d4
6-LL <lorenzanilautaro+github@gmail.com>
2026-05-21 19:01:57 -0300

    CanchaRepository findByEstablishmentIdAndActive() and findAllDistinctCanchaTypesGroupedByEstablishment() added

--- CanchaSystem/src/main/java/com/example/CanchaSystem/repository/CanchaRepository.java
+++ CanchaSystem/src/main/java/com/example/CanchaSystem/repository/CanchaRepository.java
@@ -15,15 +15,16 @@ import java.util.UUID;
 public interface CanchaRepository extends JpaRepository<Cancha,Long> {
     boolean existsById(Long id);
     Optional<Cancha> findById(Long id);
+    Optional<Cancha> findByIdAndActive(Long id, boolean active);
 
     List<Cancha> findByEstablishmentIdAndActiveAndWorking(Long id,boolean active, boolean working);
     List<Cancha> findByEstablishmentId(Long id);
+    List<Cancha> findByEstablishmentIdAndActive(Long id, boolean active);
     List<Cancha> findByActiveAndWorking(boolean active, boolean working);
     List<Cancha> findByEstablishmentIdAndCanchaType(Long establishmentId, CanchaType type);
     List<Cancha> findByEstablishment_Brand_Owner_IdAndActive(UUID ownerId, boolean active);
 
-
-    @Query("SELECT DISTINCT c.canchaType FROM Cancha c WHERE c.establishment.id = :establishmentId")
+    @Query("SELECT DISTINCT c.canchaType FROM Cancha c WHERE c.active = true AND c.working = true AND c.establishment.id = :establishmentId")
     List<CanchaType> findDistinctTypesByEstablishmentId(@Param("establishmentId") Long establishmentId);
 
     @Query("""
@@ -32,6 +33,14 @@ public interface CanchaRepository extends JpaRepository<Cancha,Long> {
     WHERE c.active = true AND c.working = true
     """)
     List<Object[]> findAllEstablishmentCanchaTypes();
+
+    @Query("""
+    SELECT c.establishment.id, c.canchaType
+    FROM Cancha c
+    WHERE c.active = true
+    GROUP BY c.establishment.id, c.canchaType
+    """)
+    List<Object[]> findAllDistinctCanchaTypesGroupedByEstablishment();
 
     List<Cancha> findByEstablishmentIdAndActiveAndWorkingAndCanchaType(
             Long establishmentId,
