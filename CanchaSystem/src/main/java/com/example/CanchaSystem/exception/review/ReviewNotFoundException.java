e2c5673ffb7d3dc347e9d26f9d433b3825f7b75a
6-LL <lorenzanilautaro+github@gmail.com>
2026-05-27 20:27:57 -0300

    ReviewNotFoundException now extends ResourceNotFoundException

--- CanchaSystem/src/main/java/com/example/CanchaSystem/exception/review/ReviewNotFoundException.java
+++ CanchaSystem/src/main/java/com/example/CanchaSystem/exception/review/ReviewNotFoundException.java
@@ -1,6 +1,8 @@
 package com.example.CanchaSystem.exception.review;
 
-public class ReviewNotFoundException extends RuntimeException {
+import com.example.CanchaSystem.exception.ResourceNotFoundException;
+
+public class ReviewNotFoundException extends ResourceNotFoundException {
     public ReviewNotFoundException(Long reviewId) {
         super("Reseña no encontrada. id= " + reviewId);
     }
