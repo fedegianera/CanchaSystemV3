3a55a38213c104f60700ef9a5251785b12ad1fb3
6-LL <lorenzanilautaro+github@gmail.com>
2026-07-26 17:19:51 -0300

    MapController endpoints updated

--- CanchaSystem/src/main/java/com/example/CanchaSystem/controller/MapController.java
+++ CanchaSystem/src/main/java/com/example/CanchaSystem/controller/MapController.java
@@ -4,13 +4,27 @@ import com.example.CanchaSystem.api.StreetLookupApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
 @RestController
 @RequestMapping("map")
 public class MapController {
-    @PostMapping("/lookup/{address}")
+    StreetLookupApi api = StreetLookupApi.getInstance();
+
+    @GetMapping("/lookup/{address}")
     public ResponseEntity<?> lookupStreet(@PathVariable String address) {
         return ResponseEntity.ok(
-                StreetLookupApi.getInstance().autocompleteAddress(address)
+                api.autocompleteAddress(
+                        URLDecoder.decode(address, StandardCharsets.UTF_8)
+                )
+        );
+    }
+
+    @GetMapping("/reversegeocoding/{lat}/{lng}")
+    public ResponseEntity<?> reverseGeocode(@PathVariable Double lat, @PathVariable Double lng) {
+        return ResponseEntity.ok(
+                api.reverseGeocodeAddress(lat, lng)
         );
     }
 }
