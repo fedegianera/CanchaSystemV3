8d067a67a87bf63246b522cecc765ef015f02b8a
6-LL <lorenzanilautaro+github@gmail.com>
2026-07-26 17:19:24 -0300

    GeoapifyApi implementation updated

--- CanchaSystem/src/main/java/com/example/CanchaSystem/api/GeoapifyApi.java
+++ CanchaSystem/src/main/java/com/example/CanchaSystem/api/GeoapifyApi.java
@@ -14,12 +14,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GeoapifyApi implements StreetLookupApi {
-    private static final String BASE_API = "https://api.geoapify.com/v1/geocode/";
+    private static final String BASE_URL = "https://api.geoapify.com/v1/geocode/";
+
     @Value("${app.geoapify-autocomplete-key}")
-    private static String API_KEY;
+    private static String AUTOCOMPLETE_KEY;
+    @Value("${app.geoapify-reversegeocoding-key}")
+    private static String REVERSEGEOCODING_KEY;
+
     private static final HttpClient client = HttpClient.newHttpClient();
 
     public static final StreetLookupApi INSTANCE = new GeoapifyApi();
+
+    @Override
+    public AddressDTO reverseGeocodeAddress(Double lat, Double lng) {
+        Params param = Params.of(
+                "lat", lat.toString(),
+                "lon", lng.toString(),
+                "lang", "es",
+                "format", "json",
+                "apiKey", REVERSEGEOCODING_KEY
+        );
+        URI endpoint = URI.create(BASE_URL + "reverse?" + param);
+
+        JsonObject obj;
+        try {
+            obj = JsonParser.parseString(
+                    client.send(
+                            HttpRequest.newBuilder()
+                                    .uri(endpoint)
+                                    .GET()
+                                    .build(),
+                            HttpResponse.BodyHandlers.ofString()
+                    ).body()
+            ).getAsJsonObject()
+                    .getAsJsonArray("features")
+                    .get(0)
+                    .getAsJsonObject()
+                    .getAsJsonObject("properties");
+        } catch (Exception e) {
+            return null;
+        }
+
+        return getAddress(obj);
+    }
+
+    private AddressDTO getAddress(JsonObject obj) {
+        double latitude = obj.get("lat").getAsDouble();
+        double longitude = obj.get("lon").getAsDouble();
+
+        String houseNumber = obj.get("house_number").getAsString();
+        String road = obj.get("road").getAsString();
+        String city = obj.get("city").getAsString();
+        String state = obj.get("state").getAsString();
+
+        String fullAddress = fullAddress(road, houseNumber, city, state);
+
+        return new AddressDTO(fullAddress, latitude, longitude);
+    }
+
+    private String fullAddress(String road, String houseNumber, String city, String state) {
+        return "%s %s, %s, %s".formatted(road, houseNumber, city, state);
+    }
 
     @Override
     public List<AddressDTO> autocompleteAddress(String text) {
@@ -28,9 +83,9 @@ public class GeoapifyApi implements StreetLookupApi {
                 "lang", "es",
                 "bias", "proximity:-38.003838,-57.556553", // Para que el test busque direcciones centradas en Mar del Plata
                 "format", "json",
-                "apiKey", API_KEY
+                "apiKey", AUTOCOMPLETE_KEY
         );
-        URI endpoint = URI.create(BASE_API + "autocomplete?" + param);
+        URI endpoint = URI.create(BASE_URL + "autocomplete?" + param);
 
         JsonArray res;
         try {
@@ -56,16 +111,7 @@ public class GeoapifyApi implements StreetLookupApi {
 
             if (isNotConfident(obj)) return;
 
-            double latitude = obj.get("lat").getAsDouble();
-            double longitude = obj.get("lon").getAsDouble();
-            String houseNumber = obj.get("house_number").getAsString();
-            String road = obj.get("road").getAsString();
-            String city = obj.get("city").getAsString();
-            String state = obj.get("state").getAsString();
-
-            String fullAddress = "%s %s, %s, %s".formatted(road, houseNumber, city, state);
-
-            addresses.add(new AddressDTO(fullAddress, latitude, longitude));
+            addresses.add(getAddress(obj));
         });
 
         return addresses;
